166bbf6d9b20de4b35c1882a85a8fd6dbeee9bfc
Abhishek Chauhan <abhishek.chauhan@connectpe.in>
2026-07-16 15:37:54 +0530

    Date : 16-07-2026 Status change global function

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,6 +89,7 @@
 
     <!-- DataTables JS -->
     <script src="https://cdn.datatables.net/2.3.2/js/dataTables.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @include('layouts.script')
     @yield('scripts')
 
