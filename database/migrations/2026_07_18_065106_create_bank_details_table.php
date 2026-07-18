e76a6ed9e833a2151d674121dddd531a2038495e
Nilam Yadav <neelam.yadav@connectpe.in>
2026-07-18 12:25:15 +0530

    create bank details migration and model

---
+++ database/migrations/2026_07_18_065106_create_bank_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bank_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('business_info_id');
+            $table->string('bank_name');
+            $table->string('account_number');
+            $table->string('account_holder_name');
+            $table->string('ifsc_code');
+            $table->string('branch_name');
+            $table->string('bank_docs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bank_details');
+    }
+};
