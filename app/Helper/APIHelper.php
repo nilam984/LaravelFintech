818989dd9b4fd7e875bcec4325ce0d03a4e07079
Abhishek Chauhan <abhishek.chauhan@connectpe.in>
2026-07-29 17:44:44 +0530

    2.1. Made ready the payin structure.
        2.2. Written the functions for extra check before making the Payin.
                checkIpWhiteListed()
                checkUserServiceActive()
                schemeAndFeeTax()
                generateReferenceId()
                defaultGateway()

--- app/Helper/APIHelper.php
+++ app/Helper/APIHelper.php
@@ -2,20 +2,28 @@
 
 namespace App\Helper;
 
+use App\Models\AssignedScheme;
 use App\Models\BussinessInfo;
+use App\Models\GatewayRouting;
 use App\Models\GlobalService;
+use App\Models\IpWhitelist;
 use App\Models\OauthUser;
+use App\Models\SchemeRules;
+use App\Models\ServiceRequest;
 use Exception;
 use Illuminate\Http\Request;
+use App\Models\PayinTransaction;
+use App\Models\PaymentGateway;
+use Illuminate\Support\Str;
 
 class APIHelper
 {
 
-    public function validateHeaderRequest(Request $request)
+    // Validate header for client ID and Secret Key
+    public function validateHeaderRequest(Request $request, string $type)
     {
 
         // dd($request->getUser(), $request->getPassword());
-
 
         if (!$request->isJson()) {
             throw new Exception('Content-Type must be application/json');
@@ -37,15 +45,15 @@ class APIHelper
 
         [$username, $password] = explode(':', $decodedCredentials, 2);
 
-        $payinService = GlobalService::where('service_name', 'Payin')->where('status', 1)->first();
+        $service = GlobalService::where('service_name', $type)->where('status', 1)->first();
 
-        if (!$payinService) {
+        if (!$service) {
             throw new Exception('Service is Temporarily Inactive');
         }
 
         $user = OauthUser::where('client_id', $username)
             ->where('client_secret', hash('sha512', $password))
-            ->where('service_id', $payinService?->id)
+            ->where('service_id', $service?->id)
             ->where('status', 1)
             ->first();
 
@@ -55,10 +63,13 @@ class APIHelper
 
         return [
             'userId' => $user->user_id,
+            'serviceId' => $service?->id
         ];
     }
 
-    public function checkKycVerified($userId)
+
+    // Check Kyc verified
+    public function checkKycVerified(int $userId)
     {
 
         $businessInfo = BussinessInfo::where('user_id', $userId)->first();
@@ -72,5 +83,146 @@ class APIHelper
         }
 
         return true;
+    }
+
+
+    // Check IP whitelisted for a specific service
+    public function checkIpWhiteListed(int $userId, int $serviceId)
+    {
+
+        $currentIp = request()->ip();
+
+        $listedIps = IpWhitelist::where('user_id', $userId)
+            ->where('service_id', $serviceId)
+            ->where('is_deleted', false)
+            ->pluck('ip')
+            ->toArray();
+
+        $listedIps = array_merge($listedIps, ['127.0.0.1', 'localhost']);
+
+
+        if (!in_array($currentIp, $listedIps)) {
+            throw new Exception("Ip not whitelisted for this service. IP : $currentIp");
+        }
+
+        return true;
+    }
+
+
+    //Check service active
+    public function checkUserServiceActive(int $userId, int $serviceId)
+    {
+
+        $serviceActive = ServiceRequest::where('user_id', $userId)
+            ->where('service_id', $serviceId)
+            ->where('status', 'active')
+            ->exists();
+
+
+        if (!$serviceActive) {
+            throw new Exception("Your Service is not Active");
+        }
+
+        return true;
+    }
+
+
+    //Check Scheme Defined
+    public function schemeAndFeeTax(int $userId, int $serviceId, $amount)
+    {
+
+        $assignedScheme = AssignedScheme::select('scheme_id')
+            ->where('user_id', $userId)
+            ->first();
+
+
+        if (!$assignedScheme?->scheme_id) {
+            throw new Exception("Scheme not Assigned Contact to Administrator");
+        }
+
+        $schemeRules = SchemeRules::where('scheme_id', $assignedScheme?->scheme_id)
+            ->where('service_id', $serviceId)
+            ->where('start_value', '<=', $amount)
+            ->where('end_value', '>=', $amount)
+            ->first();
+
+        if (!$schemeRules) {
+            throw new Exception("Scheme rule not set Contact to Administrator");
+        }
+
+
+        $fee = 0;
+        $tax = 0;
+        $finalAmount = 0;
+        $taxPercent = 18; // Static GST/Tax
+
+        if ($schemeRules) {
+
+            if ($schemeRules->fee_type === 'Percent') {
+                $fee = ($amount * $schemeRules->fee) / 100;
+            } else {
+                $fee = $schemeRules->fee;
+            }
+
+            // If calculated fee is lower than min_fee
+            if (!is_null($schemeRules->min_fee) && $fee < $schemeRules->min_fee) {
+                $fee = $schemeRules->min_fee;
+            }
+
+            // If calculated fee is maximum than max_fee
+            if (!is_null($schemeRules->max_fee) && $fee > $schemeRules->max_fee) {
+                $fee = $schemeRules->max_fee;
+            }
+
+
+            // Tax is always calculated on the fee
+            $tax = ($fee * $taxPercent) / 100;
+
+            // Final net amount after deducting fee and tax
+            $finalAmount = $amount - ($fee + $tax);
+
+            return [
+                'fee' => floatval($fee),
+                'tax' => floatval($tax),
+                'finalAmount' => floatval($finalAmount),
+            ];
+        }
+    }
+
+
+
+    public function generateReferenceId()
+    {
+        do {
+
+            $letters = chr(rand(65, 90)) . chr(rand(65, 90));
+
+            // 2. Format current time in Indian timezone (IST) with uppercase AM/PM
+            $timestampString = now('Asia/Kolkata')->format('Aymdhis') . mt_rand(100000, 999999);
+
+            $referenceId = $letters . $timestampString;
+        } while (PayinTransaction::where('payment_reference_id', $referenceId)->exists());
+
+        return $referenceId;
+    }
+
+
+    public function defaultGateway(string $type)
+    {
+        $gatewayRouting = GatewayRouting::where('gateway_type', $type)->first();
+
+
+        if (!$gatewayRouting) {
+            throw new Exception("Gateway routing not set Contact to Administrator");
+        }
+
+        $gateway = PaymentGateway::find($gatewayRouting->payment_gateway_id);
+
+        if (!$gateway) {
+            throw new Exception("Gateway not found Contact to Administrator");
+        }
+
+
+        return $gateway?->gateway_name;
     }
 }
